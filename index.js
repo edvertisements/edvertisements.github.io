cf138b2362a079bbc9455abc617596bd412e55a0
Geza Kovacs <gkovacs@stanford.edu>
2015-09-16 12:24:08 -0700

    updated coming soon links

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 (function(){
   var install_edvertisements_web, install_edvertisements_feed, download_source, out$ = typeof exports != 'undefined' && exports || this;
   out$.install_edvertisements_web = install_edvertisements_web = function(){
-    return alert('Coming soon');
+    return alert('Coming soon (paper is undergoing review but extension is not anonymized)');
   };
   out$.install_edvertisements_feed = install_edvertisements_feed = function(){
-    return alert('Coming soon');
+    return alert('Coming soon (paper is undergoing review but extension is not anonymized)');
   };
   out$.download_source = download_source = function(){
-    return alert('Coming soon');
+    return alert('Coming soon (paper is undergoing review but source code is not anonymized)');
   };
 }).call(this);
--- index.ls
+++ index.ls
@@ -1,8 +1,8 @@
 export install_edvertisements_web = ->
-  alert 'Coming soon'
+  alert 'Coming soon (paper is undergoing review but extension is not anonymized)'
 
 export install_edvertisements_feed = ->
-  alert 'Coming soon'
+  alert 'Coming soon (paper is undergoing review but extension is not anonymized)'
 
 export download_source = ->
-  alert 'Coming soon'
+  alert 'Coming soon (paper is undergoing review but source code is not anonymized)'
